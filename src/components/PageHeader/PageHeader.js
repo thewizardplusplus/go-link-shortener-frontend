1190eb47a5da7347cf6f4e0c71662148eab14cdf
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-04-14 23:00:00 +0300

    Add the Home button

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { PageHeader as AntPageHeader, Tag } from 'antd'
+import { HomeOutlined } from '@ant-design/icons'
+import { useHistory } from 'react-router-dom'
 import packageInfo from '../../../package.json'
 import './PageHeader.css'
 
 export function PageHeader() {
+  const history = useHistory()
   return (
     <AntPageHeader
-      backIcon="false"
+      backIcon={<HomeOutlined />}
       title="go-link-shortener"
       tags={<Tag>{packageInfo.version}</Tag>}
+      onBack={() => history.push('/')}
     />
   )
 }
