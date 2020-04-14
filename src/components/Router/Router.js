8f4493d7441ebe7558a9c60ac83c76fcdae9baf2
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-04-14 23:00:00 +0300

    Remove navigation components

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
-import { Navigation } from '../Navigation/Navigation.js'
-import { NavigationButton } from '../NavigationButton/NavigationButton.js'
 import { Form } from '../Form/Form.js'
 import { Success } from '../Success/Success.js'
 import { Error } from '../Error/Error.js'
@@ -9,26 +7,20 @@ import { Error } from '../Error/Error.js'
 export function Router() {
   return (
     <BrowserRouter>
-      <div>
-        <Navigation />
-
-        <Switch>
-          <Route exact path="/">
-            <span>Home</span>
-            <NavigationButton />
-            <Form />
-          </Route>
-          <Route path="/success/:code">
-            <Success />
-          </Route>
-          <Route path="/error">
-            <Error />
-          </Route>
-          <Route>
-            <Redirect to="/error" />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <Form />
+        </Route>
+        <Route path="/success/:code">
+          <Success />
+        </Route>
+        <Route path="/error">
+          <Error />
+        </Route>
+        <Route>
+          <Redirect to="/error" />
+        </Route>
+      </Switch>
     </BrowserRouter>
   )
 }
