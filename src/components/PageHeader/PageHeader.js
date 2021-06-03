fd436baf0a4bb2ed0d3c83497148cde060d6a54a
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-03 23:00:00 +0300

    Make the home page icon a link to it

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { PageHeader as AntPageHeader, Tag } from 'antd'
 import { HomeOutlined } from '@ant-design/icons'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import packageInfo from '../../../package.json'
 import './PageHeader.css'
 
 export function PageHeader() {
-  const history = useHistory()
   return (
     <AntPageHeader
-      backIcon={<HomeOutlined />}
+      backIcon={
+        <Link to="/">
+          <HomeOutlined />
+        </Link>
+      }
       title="go-link-shortener"
       tags={<Tag>{packageInfo.version}</Tag>}
-      onBack={() => history.push('/')}
+      onBack={() => null}
     />
   )
 }
