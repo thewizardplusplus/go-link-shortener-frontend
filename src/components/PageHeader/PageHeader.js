5a1fa67b84d60206fe24e8c884806dbee4502ec0
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-06-03 23:00:00 +0300

    Add the tooltips for the home page links

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PageHeader as AntPageHeader, Tag } from 'antd'
+import { PageHeader as AntPageHeader, Tooltip, Tag } from 'antd'
 import { HomeOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import packageInfo from '../../../package.json'
@@ -9,11 +9,17 @@ export function PageHeader() {
   return (
     <AntPageHeader
       backIcon={
-        <Link to="/">
-          <HomeOutlined />
-        </Link>
+        <Tooltip title="Home page">
+          <Link to="/">
+            <HomeOutlined />
+          </Link>
+        </Tooltip>
       }
-      title={<Link to="/">{'go-link-shortener'}</Link>}
+      title={
+        <Tooltip title="Home page">
+          <Link to="/">{'go-link-shortener'}</Link>
+        </Tooltip>
+      }
       tags={<Tag>{packageInfo.version}</Tag>}
       onBack={() => null}
     />
